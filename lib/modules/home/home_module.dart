5d292c6bf6861d7b1d5aaeeb707d027c38e33d2b
Vinicius Porto <vinicius.p.a.corin@gmail.com>
2025-01-07 09:26:55 -0300

    added add tasks page

--- lib/modules/home/home_module.dart
+++ lib/modules/home/home_module.dart
@@ -1,3 +1,4 @@
+import 'package:clean_nest/modules/home/src/ui/pages/add_task_page.dart';
 import 'package:clean_nest/modules/home/src/ui/pages/home_page.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -8,5 +9,6 @@ class HomeModule extends Module {
   @override
   void routes(r) {
     r.child('/', child: (context) => const HomePage());
+    r.child('/add_task', child: (context) => const AddTaskPage());
   }
 }
--- lib/modules/home/src/ui/pages/home_page.dart
+++ lib/modules/home/src/ui/pages/home_page.dart
@@ -1,16 +1,61 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:clean_nest/core/themes/theme_spacings.dart';
+import 'package:clean_nest/core/themes/theme_text_styles.dart';
+import 'package:clean_nest/core/themes/themes.dart';
+import 'package:clean_nest/shared/widgets/cn_appbar_widget.dart';
+import 'package:clean_nest/shared/widgets/cn_scaffold_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
+    final theme = Theme.of(context);
+    final themeTextStyle = theme.extension<CnTextStyles>();
+    final themeSpacing = theme.extension<CnSpacing>();
+    final size = MediaQuery.of(context).size;
+    int _selectedIndex = 0;
+    List<IconData> listIcon = [Icons.home, Icons.settings, Icons.person];
+    final List<Widget> _pages = [
+      Container(),
+      Container(),
+      Container(),
+    ];
+    return CnScaffoldWidget(
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        icons: listIcon,
+        activeIndex: _selectedIndex,
+        activeColor: cnColorScheme.primary,
+        blurEffect: true,
+        elevation: 20,
+        height: 65,
+        iconSize: 24,
+        inactiveColor: Colors.white,
+        // rightCornerRadius: 12,
+        // leftCornerRadius: 12,
+        splashColor: const Color(0xff424242),
+        // backgroundColor: const Color(0xff424242),
+        onTap: (index) => setState(() {
+          _selectedIndex = index;
+        }),
       ),
-      body: const Center(
-        child:  Text('Home Page'),
+      appBar: const CnAppBarWidget(title: 'Home Page', showBackButton: true),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: IconButton(
+                onPressed: () => Modular.to.pushNamed('add_task'),
+                icon: Icon(Icons.add)),
+          ),
+        ],
       ),
     );
   }
