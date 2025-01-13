c9726f4594fa1e6351c1ea50ed38d6b79a682c63
Vinicius Porto <vinicius.p.a.corin@gmail.com>
2025-01-13 11:48:22 -0300

    added local storage with shared preference in global project

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:clean_nest/app_module.dart';
 import 'package:clean_nest/app_widget.dart';
+import 'package:clean_nest/core/utils/shared_pref.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SharedPref.init();
+
   runApp(
     ModularApp(
       module: AppModule(),
-      child:  AppWidget(),
+      child: AppWidget(),
     ),
   );
 }
