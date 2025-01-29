0ec0d72debc6184d9af239339e7caf153c172ec4
Vinicius Porto <vinicius.p.a.corin@gmail.com>
2025-01-29 10:53:40 -0300

    changed new mascot asset

--- lib/modules/auth/src/data/repositories/choose_mascot_repository_impl.dart
+++ lib/modules/auth/src/data/repositories/choose_mascot_repository_impl.dart
@@ -5,9 +5,12 @@ class ChooseMascotRepositoryImpl implements ChooseMascotRepository {
   @override
   List<Mascot> fetchMascots() {
     return [
-      Mascot(id: 1, name: 'Mascot A', imageUrl: 'assets/images/mascot.jpg'),
-      Mascot(id: 2, name: 'Mascot B', imageUrl: 'assets/images/mascot.jpg'),
-      Mascot(id: 3, name: 'Mascot C', imageUrl: 'assets/images/mascot.jpg'),
+      Mascot(
+          id: 1, name: 'Mascot A', imageUrl: 'assets/images/logo_elizeu.png'),
+      Mascot(
+          id: 2, name: 'Mascot B', imageUrl: 'assets/images/logo_elizeu.png'),
+      Mascot(
+          id: 3, name: 'Mascot C', imageUrl: 'assets/images/logo_elizeu.png'),
     ];
   }
 }
